8fbfa01659b419ee2662a4d4692a28a93ed9edb8
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-05-25 17:59:21 +0530

    Basic Server Setup and Auth Middleware w/ Firebase SDK

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const authMiddleware = require("./auth-middleware");
+
+const app = express();
+app.use(cors());
+
+const data = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    image:
+      "https://pmpub-catalogue.s3-eu-west-1.amazonaws.com/covers/web/9781781100240.jpg",
+  },
+  {
+    id: 2,
+    name: "Clean Code",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX374_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 3,
+    name: "Javascript: The good parts",
+    image: "https://images-na.ssl-images-amazon.com/images/I/81kqrwS1nNL.jpg",
+  },
+];
+
+app.use("/", authMiddleware);
+
+app.post("/conversations", (request, response) => {
+  return response.send({ data });
+});
+
+app.listen(4000, () => console.log("The server is running at PORT 4000"));
